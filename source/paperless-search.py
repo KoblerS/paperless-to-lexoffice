1fa388c2d283e7ebc7ee99d2cb5906a8cea745ae
Simon Kobler <github@kobler.me>
2025-08-22 10:00:13 +0200

    Enhance README and environment configuration; improve error handling and logging in lexoffice integration

--- source/paperless-search.py
+++ source/paperless-search.py
@@ -11,9 +11,12 @@ INBOX_TAG_ID = os.getenv('INBOX_TAG_ID')
 LEXOFFICE_TAG_ID = os.getenv('LEXOFFICE_TAG_ID')
 LEXOFFICE_USERNAME = os.getenv('LEXOFFICE_USERNAME')
 LEXOFFICE_PASSWORD = os.getenv('LEXOFFICE_PASSWORD')
+PAPERLESS_ADD_VOUCHER_URL = bool(os.getenv('PAPERLESS_ADD_VOUCHER_URL', 'True'))
 
 TMP_DIR = "tmp"
 LOCK_FILE = 'script.lock'
+
+LEXOFFICE_VOUCHER_PREVIEW_URL = "https://app.lexware.de/vouchers#!/VoucherList/Invoice/"
 
 def create_lock():
     with open(LOCK_FILE, 'w') as f:
@@ -28,12 +31,12 @@ def is_locked():
 
 async def sync_paperless_to_lexoffice():
     if is_locked():
-        print("Script is already running. Exiting.")
+        print("[Sync] Script is already running. Exiting.")
         return
 
     create_lock()
     try:
-        print("Checking for new documents in paperless-ngx tagged for upload...")
+        print("[Sync] Checking for new documents in paperless-ngx tagged for upload...")
         document_ids = paperless.filter_documents_by_tags(
             PAPERLESS_TOKEN, PAPERLESS_URL, [INBOX_TAG_ID, LEXOFFICE_TAG_ID]
         )
@@ -46,21 +49,23 @@ async def sync_paperless_to_lexoffice():
                 with open(filepath, "wb") as file:
                     file.write(file_content)
 
-                response = lexoffice.upload_voucher(
+                lexoffice_document_uuid = lexoffice.upload_voucher(
                     filepath,
                     username=LEXOFFICE_USERNAME,
                     password=LEXOFFICE_PASSWORD
                 )
 
-                if response.status_code == 200:
-                    print("Upload successful. Deleting file from tmp...")
-                    os.remove(filepath)
+                os.remove(filepath)
+
+                if lexoffice_document_uuid:
+                    print("[Sync] Upload successful. Deleting file from tmp...")
                     paperless.remove_tag(PAPERLESS_TOKEN, PAPERLESS_URL, doc_id, [LEXOFFICE_TAG_ID])
+                    paperless.set_custom_field(PAPERLESS_TOKEN, PAPERLESS_URL, doc_id, "lexoffice_document_uuid", f"{LEXOFFICE_VOUCHER_PREVIEW_URL}{lexoffice_document_uuid}")
                 else:
-                    print(f"Upload not successful. Leave file in tmp. HTTP error {response.status_code}")
+                    print(f"[Sync] Upload not successful. Deleting file from tmp as it gets downloaded in the next cycle.")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"[Sync] An error occurred: {e}")
     finally:
         remove_lock()
 
